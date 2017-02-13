3b54a885ee8d4286f131c8d98f3b68a880b1adc0
2a6m <maxime.bourguignon1996@gmail.com>
2017-02-13 21:57:08 +0100

    Add more test

--- test_utils.py
+++ test_utils.py
@@ -8,15 +8,22 @@ import utils
 class TestUtils(unittest.TestCase):
     def test_fact(self):
         self.assertEqual(utils.fact(0), 1)
-        self.assertEqual(utils.fact('blabla'), -1)
         self.assertEqual(utils.fact(3), 6)
+        with self.assertRaises(ValueError):
+            utils.fact(-5)
+            utils.fact('blabla')
     
     def test_roots(self):
-        self.assertEqual(utils.roots(0, 0, 0), (0, 0))
-        self.assertEqual(utils.roots(0, 1, 0), (0, 0))
+        self.assertEqual(utils.roots(0, 1, 0), (0))
+        self.assertEqual(utils.roots(0, 0, 0), (0))
+        self.assertEqual(utils.roots(4, 7, 3), (-1, -0.75))
     
     def test_integrate(self):
-        self.assertEqual(utils.integrate(1, 0, 1), 1)
+        self.assertEqual(utils.integrate('x', 0, 1), 0.5)
+        self.assertEqual(utils.integrate('1', 0, 1), 1)
+        self.assertEqual(utils.integrate('x', -1, 1), 0)
+
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
