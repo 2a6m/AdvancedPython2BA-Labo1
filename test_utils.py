a56ea22346b03abe5b61f760c24091ef8799b11d
2a6m <maxime.bourguignon1996@gmail.com>
2017-02-13 15:14:54 +0100

    correct test

--- test_utils.py
+++ test_utils.py
@@ -16,7 +16,7 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(utils.roots(0, 1, 0), 1)
     
     def test_integrate(self):
-        self.assertEqual(utils.integrate(1, 0, 1), 0.5)
+        self.assertEqual(utils.integrate(1, 0, 1), 1)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
