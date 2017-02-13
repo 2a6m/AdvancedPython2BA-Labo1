bb176dd1e0a86134d8864ae0f53f38baf223bbe6
2a6m <maxime.bourguignon1996@gmail.com>
2017-02-13 16:05:17 +0100

    correct roots module and fact module correction (3)

--- utils.py
+++ utils.py
@@ -2,40 +2,49 @@
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
 
+import math as m
+
 def fact(n):
-    # Python program to find the factorial of a number provided by the user.
-
-    # change the value for a different result
-
-    # uncomment to take input from the user
-    # num = int(input("Enter a number: "))
-
-    factorial = 1
-
-    # check if the number is negative, positive or zero
-    if n < 0:
-        print("Sorry, factorial does not exist for negative numbers")
-    elif n == 0:
-        print("The factorial of 0 is 1")
-    else:
-        for i in range(1, n + 1):
-            factorial = factorial * i
-        print("The factorial of", n, "is", factorial)
-
+    """Computes the factorial of a natural number.
+    
+    Pre: -
+    Post: Returns the factorial of 'n'.
+    Throws: ValueError if n < 0
+    """
+    return m.factorial(n)
 
 def roots(a, b, c):
-    assert (int(a), int(b), int(c))
-    delta = b**2 - 4*a*c
+    """Computes the roots of the ax^2 + bx + x = 0 polynomial.
+    
+    Pre: -
+    Post: Returns a tuple with zero, one or two elements corresponding
+          to the roots of the ax^2 + bx + c polynomial.
+    """
+    assert (int(a))
+    assert (int(b))
+    assert (int(c))
+    delta = b ** 2 - 4 * a * c
     if a == 0:
-        r = (-b + delta**(1/2))
-        return r
-    r = (-b + delta**(1/2) / a)
-    return r
+        rp = (-b + m.sqrt(delta))
+        rn = (-b - m.sqrt(delta))
+        return rp, rn
+    rp = (-b + m.sqrt(delta) / a)
+    rn = (-b - m.sqrt(delta) / a)
+    return rp, rn
 
 def integrate(function, lower, upper):
+    """Approximates the integral of a fonction between two bounds
+    
+    Pre: 'function' is a valid Python expression with x as a variable,
+         'lower' <= 'upper',
+         'function' continuous and integrable between 'lower‘ and 'upper'.
+    Post: Returns an approximation of the integral from 'lower' to 'upper'
+          of the specified 'function'.
+    """
     return 1
 
 if __name__ == '__main__':
     print(fact(5))
     print(roots(1, 0, 1))
     print(integrate('x ** 2 - 1', -1, 1))
+
